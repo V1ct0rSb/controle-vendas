3d5e4dd15f19fd302f6424dff80532bb21c83680
Victor Barreto <112266976+V1ct0rSb@users.noreply.github.com>
2024-03-14 12:41:53 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'controle.vendas',
+  appName: 'controle-vendas',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
