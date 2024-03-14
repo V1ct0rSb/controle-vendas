42c7e4d6f01017c2caa29b0a8a46fbf8e0dbd6b5
Victor Barreto <112266976+V1ct0rSb@users.noreply.github.com>
2024-03-14 12:46:32 -0300

    Iniciando o projeto

---
+++ android/app/src/main/java/controle/vendas/MainActivity.java
@@ -0,0 +1,5 @@
+package controle.vendas;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'controle.vendas',
   appName: 'controle-vendas',
-  webDir: 'www',
+  webDir: 'www/browser',
   server: {
     androidScheme: 'https'
   }
