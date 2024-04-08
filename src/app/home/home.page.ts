130d80ab6442b8ab0d2b81c107eb20813625b279
Victor Barreto <112266976+V1ct0rSb@users.noreply.github.com>
2024-04-08 12:53:10 -0300

    Atualiza botão de sair para exibir um alerta de confirmação antes de redirecionar para a página de login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import {
   IonHeader,
   IonToolbar,
   IonTitle,
   IonContent,
   IonButton,
-  IonIcon
+  IonIcon,
 } from '@ionic/angular/standalone';
 
 @Component({
@@ -17,9 +18,33 @@ import {
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon],
 })
 export class HomePage {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private alertController: AlertController
+  ) {}
 
   navegarPara(rota: string) {
     this.router.navigateByUrl(rota);
+  }
+
+  async confirmarSaida() {
+    const alert = await this.alertController.create({
+      header: 'Confirmação',
+      message: 'Você realmente deseja sair?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+        },
+        {
+          text: 'Sair',
+          handler: () => {
+            this.navegarPara('login');
+          },
+        },
+      ],
+    });
+    await alert.present();
   }
 }
